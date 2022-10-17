6745577a94538a1a410d7ffab914dbaf88cd8aa4
dmorn <danielmorandini@me.com>
2022-10-17 21:17:57 +0200

    Add basic read loop

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
+const stdin = std.io.getStdIn().reader();
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+pub fn main() !void {
+    var buffer: [256]u8 = undefined;
+    while (true) {
+        var size = try stdin.read(&buffer);
+        std.debug.print("#{d} bytes were read: {s}", .{ size, buffer });
+    }
 }
